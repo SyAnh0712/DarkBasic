cfab9d3e4177541448aed1353327e54b039b582f
SyAnh0712 <syanh0712@gmail.com>
2026-06-03 23:37:47 +0700

    lazy Initialization

---
+++ vidu_04.dart
@@ -0,0 +1,18 @@
+/* Biến toàn cục, phai su dung ? 
+hoac khai bao gia tri cho bien toan cuc
+ de tranh bi loi null safety
+*/
+// String moTa;
+// String? moTa;
+
+// khai bao theo kieu lazy Initialization
+// nghia la xiu nua moi gan gia tri cho bien toan cuc
+late String moTa;
+
+void main() {
+  // muon moTa co gia tri null thi late Strig? moTa;
+  // moTa = null;
+
+  moTa = "Flutter la mot framework tot";
+  print(moTa);
+}
