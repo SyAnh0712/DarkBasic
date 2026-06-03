b27d3dff74b37603945264f94cc6cb11475b22d6
SyAnh0712 <syanh0712@gmail.com>
2026-06-03 23:30:46 +0700

    null safety

---
+++ vidu_03.dart
@@ -0,0 +1,12 @@
+void main() {
+  //null safety: khai bao bien khong duoc null
+  // String ten;
+  // ten = null;
+
+  // dung dau ? de khai bao bien co the null
+  String? ten;
+  ten = null;
+
+  // khong su dung duoc null voi var
+  // var? tuoi = 18;
+}
