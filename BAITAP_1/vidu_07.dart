553c72e6a4b75c8315bd5f83b4b4cf24c1bba93f
SyAnh0712 <syanh0712@gmail.com>
2026-06-05 23:32:32 +0700

    phep tang giam

---
+++ BAITAP_1/vidu_07.dart
@@ -0,0 +1,9 @@
+void main() {
+  // phep tang giam
+  int y = 5;
+  print(y++); // in 5 , y = 6
+  print(++y); // in 7 , y = 7
+  print(y--); // in 7 , y = 6
+  print(--y); // in 5 , y = 5
+  print(y); // in 5 , y = 5
+}
