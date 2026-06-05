c51d616582fb826b63ec31aab8f769942088fd49
SyAnh0712 <syanh0712@gmail.com>
2026-06-05 22:43:47 +0700

    cac phep tinh co ban

---
+++ BAITAP_1/vidu_06.dart
@@ -0,0 +1,24 @@
+void main() {
+  // cac phep tinh co ban
+  int a = 5 + 3;
+  int b = 10 - 4;
+  int c = 3 * 4;
+  double d = 10 / 2;
+  int e = 7 % 3;
+  print(d);
+
+  //phep chia lay phan nguyen
+  int f = 7 ~/ 3;
+  print(f);
+
+  // phep gan va tinh toan ket hop
+  int x = 10;
+  x += 5; // x = x + 5
+  print(x);
+  x -= 3; // x = x - 3
+  print(x);
+  x *= 2; // x = x * 2
+  print(x);
+  x ~/= 4; // x = x ~/ 4
+  print(x);
+}
