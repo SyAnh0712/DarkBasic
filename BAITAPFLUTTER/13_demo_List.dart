2d6be9c17bb41654cb1ad4fa8f79e64408549a8b
SyAnh0712 <syanh0712@gmail.com>
2026-06-30 13:56:23 +0700

    demo list

---
+++ BAITAPFLUTTER/13_demo_List.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> names = ["Raj", "John", "Max"];
+  print("Value of names is $names");
+  print("Value of names is ${names[0]}");
+  print("Value of names is ${names[1]}");
+  print("Value of names is ${names[2]}");
+
+  int length = names.length;
+  print("the length of name is $length");
+}
