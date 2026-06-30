a0c69d88b764e46929f52a0f1f1f8568af6e3e36
SyAnh0712 <syanh0712@gmail.com>
2026-06-30 14:13:11 +0700

    demo list 2

--- BAITAPFLUTTER/13_demo_List.dart
+++ BAITAPFLUTTER/13_demo_List.dart
@@ -1,9 +1,26 @@
+import '../BAITAP_1/vidu_05.dart';
+
 void main() {
   List<String> names = ["Raj", "John", "Max"];
   print("Value of names is $names");
   print("Value of names is ${names[0]}");
   print("Value of names is ${names[1]}");
   print("Value of names is ${names[2]}");
+
+  names.add("Steve Job");
+  print("Value of names is $names");
+
+  names.insert(2, "Anh");
+  print("Value of names is $names");
+
+  names.remove("Anh");
+  print("Value of names is $names");
+
+  // names.clear();
+  // print("Value of names is $names");
+
+  print(names.contains("John"));
+  print(names.indexOf("John"));
 
   int length = names.length;
   print("the length of name is $length");
