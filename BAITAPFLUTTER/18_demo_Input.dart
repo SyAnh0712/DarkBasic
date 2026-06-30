370aec6495125fb01b317d45d433ed4b4511f71c
SyAnh0712 <syanh0712@gmail.com>
2026-06-30 15:54:31 +0700

    demo input

--- BAITAPFLUTTER/18_demo_Input.dart
+++ BAITAPFLUTTER/18_demo_Input.dart
@@ -4,4 +4,8 @@ void main() {
   print("enter name: ");
   String? name = stdin.readLineSync();
   print("the entered name is ${name}");
+
+  print("enter number:");
+  int? number = int.parse(stdin.readLineSync()!);
+  print("the entered number is ${number}");
 }
