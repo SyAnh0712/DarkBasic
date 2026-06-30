ee4548c35e27a76377cf73ff17b3ad9549a452f2
SyAnh0712 <syanh0712@gmail.com>
2026-06-30 13:51:36 +0700

    demo kiem tra kieu du lieu 2

---
+++ BAITAPFLUTTER/12_demo_KiemTraKieuDuLieu_2.dart
@@ -0,0 +1,7 @@
+void main() {
+  double num1 = 10.01;
+  int num2 = num1.toInt();
+
+  print("the value of num1 is $num1. Its type is ${num1.runtimeType}");
+  print("the value of num1 is $num2. Its type is ${num1.runtimeType}");
+}
