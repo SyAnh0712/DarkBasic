206bcec07f6e11aca40ec6629ee8b417b8de7471
SyAnh0712 <syanh0712@gmail.com>
2026-07-05 20:33:12 +0700

    update bai tap 04

--- BAITAPVENHA_01/04_TinhLaiSuatDon.dart
+++ BAITAPVENHA_01/04_TinhLaiSuatDon.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 
 void main() {
   print("Nhap so tien goc:");
-  double p = double.parse(stdin.readLineSync()!);
+  double p = double.parse(stdin.readLineSync()!); // VND
 
   print("Nhap thoi gian:");
-  double t = double.parse(stdin.readLineSync()!);
+  double t = double.parse(stdin.readLineSync()!); // nam
 
   print("Nhap lai suat:");
-  double r = double.parse(stdin.readLineSync()!);
+  double r = double.parse(stdin.readLineSync()!); // %/nam
 
   double interest = (p * t * r) / 100;
 
