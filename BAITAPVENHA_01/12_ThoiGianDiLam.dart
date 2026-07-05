0487041570183a869731c828ea601a605bd6500d
SyAnh0712 <syanh0712@gmail.com>
2026-07-05 20:31:15 +0700

    bai tap 12 thoi gian di lam

---
+++ BAITAPVENHA_01/12_ThoiGianDiLam.dart
@@ -0,0 +1,10 @@
+void main() {
+  double distance = 25;
+  double speed = 40;
+
+  double timeHour = distance / speed;
+
+  double timeMinute = timeHour * 60;
+
+  print("Thoi gian di den van phong: $timeMinute phut");
+}
