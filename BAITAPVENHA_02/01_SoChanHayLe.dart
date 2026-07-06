f0073308d3ec48a63d7f437fc4f23de6ec7cd35e
SyAnh0712 <syanh0712@gmail.com>
2026-07-06 20:26:04 +0700

    bai tap(02) 01 so chan hay le

---
+++ BAITAPVENHA_02/01_SoChanHayLe.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print("Nhap mot so nguyen:");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number % 2 == 0) {
+    print("$number la so chan.");
+  } else {
+    print("$number la so le.");
+  }
+}
