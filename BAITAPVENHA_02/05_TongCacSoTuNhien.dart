2e2e1875231941a13fb3223ac3e6e0aaf8b519e3
SyAnh0712 <syanh0712@gmail.com>
2026-07-07 07:47:17 +0700

    bai tap(02) 05 tong cac so tu nhien

---
+++ BAITAPVENHA_02/05_TongCacSoTuNhien.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print("Nhap n:");
+  int n = int.parse(stdin.readLineSync()!);
+
+  int sum = 0;
+
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  print("Tong = $sum");
+}
