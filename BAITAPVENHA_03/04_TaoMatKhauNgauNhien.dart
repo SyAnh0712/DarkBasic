3843ba3c3a5f07e429713e2428b23ff448beff2d
SyAnh0712 <syanh0712@gmail.com>
2026-07-07 15:01:01 +0700

    bai tap(03) 04 tao mat khau ngau nhien

---
+++ BAITAPVENHA_03/04_TaoMatKhauNgauNhien.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+void main() {
+  int password1 = Random().nextInt(900000) + 100000;
+  print("Your new password is: $password1");
+
+  List<int> password2 = List.generate(6, (_) => Random().nextInt(10));
+  print("Your new password is: $password2");
+}
