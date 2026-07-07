cac5ed5b34d0fd58defc65ea200acd75efe675cc
SyAnh0712 <syanh0712@gmail.com>
2026-07-07 16:15:09 +0700

    bai tap(03) update all 3

--- BAITAPVENHA_03/09_SoLonNhat.dart
+++ BAITAPVENHA_03/09_SoLonNhat.dart
@@ -3,10 +3,15 @@ import 'dart:math';
 
 double maxNumber(double a, double b, double c) => max(max(a, b), c);
 
+double maxNumber2(double a, double b, double c) =>
+    a > b ? (a > c ? a : c) : (b > c ? b : c);
+
 void main() {
   double a = double.parse(stdin.readLineSync()!);
   double b = double.parse(stdin.readLineSync()!);
   double c = double.parse(stdin.readLineSync()!);
   double result = maxNumber(a, b, c);
+  double result2 = maxNumber2(a, b, c);
   print("The maximum number is: $result");
+  print("The maximum number 2 is: $result2");
 }
