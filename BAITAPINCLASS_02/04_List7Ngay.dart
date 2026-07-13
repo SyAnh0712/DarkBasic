b84200c499490d0fccc73c8483f864fd0d298523
SyAnh0712 <syanh0712@gmail.com>
2026-07-13 10:07:20 +0700

    bai tap in class(02) update 04 addAll()

--- BAITAPINCLASS_02/04_List7Ngay.dart
+++ BAITAPINCLASS_02/04_List7Ngay.dart
@@ -1,13 +1,23 @@
 void main() {
   List<String> days = [];
 
-  days.add("Monday");
-  days.add("Tuesday");
-  days.add("Wednesday");
-  days.add("Thursday");
-  days.add("Friday");
-  days.add("Saturday");
-  days.add("Sunday");
+  // days.add("Monday");
+  // days.add("Tuesday");
+  // days.add("Wednesday");
+  // days.add("Thursday");
+  // days.add("Friday");
+  // days.add("Saturday");
+  // days.add("Sunday");
+
+  days.addAll([
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ]);
 
   for (String day in days) {
     print(day);
