64d81c1109162eafe1ad1656de4cd5e94b360e65
SyAnh0712 <syanh0712@gmail.com>
2026-07-13 10:53:18 +0700

    demo file 3

--- BAITAPFLUTTER/36_demo_File.dart
+++ BAITAPFLUTTER/36_demo_File.dart
@@ -6,6 +6,10 @@ void main() {
   String contents = file.readAsStringSync();
 
   file.writeAsStringSync("Wellcome to text.txt file!");
+  file.writeAsStringSync(
+    contents + "\nThis is a new line added to the file.",
+    mode: FileMode.append,
+  );
   print("File created successfully.");
 
   print("File content: $contents");
@@ -20,6 +24,14 @@ void main() {
 
   String contentscsv = filecsv.readAsStringSync();
 
+  filecsv.writeAsStringSync("Name, Phone\n");
+  for (int i = 0; i < 3; i++) {
+    stdout.write("Enter name of student ${i + 1}: ");
+    String? name = stdin.readLineSync()!;
+    stdout.write("Enter phone of student ${i + 1}: ");
+    String? phone = stdin.readLineSync()!;
+    filecsv.writeAsStringSync("$name, $phone\n", mode: FileMode.append);
+  }
   List<String> lines = contentscsv.split('\n');
   for (String line in lines) {
     print(line);
