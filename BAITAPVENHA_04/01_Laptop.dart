fd033cdbddf9b29fb0cc20d6c570011cbdb5a396
SyAnh0712 <syanh0712@gmail.com>
2026-07-18 22:42:01 +0700

    bai tap ve nha(03): 01 laptop

---
+++ BAITAPVENHA_04/01_Laptop.dart
@@ -0,0 +1,24 @@
+class Laptop {
+  int id;
+  String name;
+  int ram;
+
+  Laptop(this.id, this.name, this.ram);
+
+  void display() {
+    print("ID: $id");
+    print("Name: $name");
+    print("RAM: ${ram}GB");
+    print("----------------");
+  }
+}
+
+void main() {
+  Laptop l1 = Laptop(1, "Dell", 8);
+  Laptop l2 = Laptop(2, "Asus", 16);
+  Laptop l3 = Laptop(3, "HP", 32);
+
+  l1.display();
+  l2.display();
+  l3.display();
+}
